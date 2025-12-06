026b2fe1305972c742c4ccd89894e3860caef2f2
Yui <529247970@qq.com>
2025-12-06 23:16:49 +0800

    feat(api): 添加鱼乎相关的API请求函数

---
+++ src/main/resources/skins/classic/yuhu/js/api.js
@@ -0,0 +1,55 @@
+function yuhuAPI(url, options) {
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.code == 0) {
+          resolve(data);
+        } else {
+          reject(data);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+// 创建鱼乎
+export const createYuhu = async (
+  title,
+  intro,
+  authorProfileId,
+  coverURL,
+  tags
+) => {
+  return yuhuAPI("/yuhu/book", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      intro: intro,
+      authorProfileId: authorProfileId,
+      coverURL: coverURL,
+      tags: tags,
+    }),
+  });
+};
+
+// 查询鱼乎列表
+export const queryYuhu = async (tag,q,sort,page,size) => {
+    const params = new URLSearchParams();
+    params.append("tag", tag);
+    params.append("q", q);
+    params.append("sort", sort);
+    params.append("page", page);
+    params.append("size", size);
+  return yuhuAPI(`/yuhu/books/${params.toString()}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
